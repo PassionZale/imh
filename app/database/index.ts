e9e0def111d84c010635765c54523354feb9146e
Zhang Lei <372470745@qq.com>
2024-07-16 09:56:41 +0800

    cwd

--- app/database/index.ts
+++ app/database/index.ts
@@ -1,12 +1,9 @@
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { join } from "node:path";
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 import { Tables } from "@/app/interfaces";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const file = join(__dirname, "db.json");
+const file = join(process.cwd(), "/app/database/db.json");
 
 const defaultData: Tables = {
   users: [
